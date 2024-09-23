1926085808d0160204e89c6cdc89e1018770917d
ck-nm <168258507+ck-nm@users.noreply.github.com>
2024-09-23 19:49:57 +0000

    bootstrap

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,3 @@
 class Public::PostsController < ApplicationController
+  before_action :authenticate_public!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,25 @@
 Rails.application.routes.draw do
 
-# 顧客用
-# URL /customers/sign_in ...
-devise_for :publics,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :publics,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  namespace :public do
+    resources :posts # public/post
+    get "search" => 'searches#search' # public/search
+  end
+  
+  namespace :admin do
+    resources :posts
+  end
 
 end
